c87947719ac4e9538e97bb8990402056389167ae
Alaa Kracah <alaaqaraja2004@gmail.com>
2025-03-09 21:02:57 +0300

    dd

--- .next/types/app/layout.ts
+++ .next/types/app/layout.ts
@@ -1,4 +1,4 @@
-// File: C:\Users\jamal\Downloads\port\app\layout.tsx
+// File: C:\Users\Alaa Kracah\OneDrive\سطح المكتب\portofolio-template-main\app\layout.tsx
 import * as entry from '../../../app/layout.js'
 import type { ResolvingMetadata, ResolvingViewport } from 'next/dist/lib/metadata/types/metadata-interface.js'
 
